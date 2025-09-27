0a795ec4a995963d6b7eab1aaf58455f4d67ec60
shawjitesh <jiteshshaw93@gmail.com>
2025-09-27 13:12:09 +0530

    MergeStringsAlternately

--- Array/String/MergeStringsAlternately.java
+++ Array/String/MergeStringsAlternately.java
@@ -1,5 +1,20 @@
 package Array.String;
 
+import java.util.stream.IntStream;
+
 public class MergeStringsAlternately {
+
+    public String mergeAlternately(String word1, String word2) {
+        return IntStream.range(0, Math.max(word1.length(), word2.length()))
+            .mapToObj(i -> 
+                (i < word1.length() ? String.valueOf(word1.charAt(i)) : "") +
+                (i < word2.length() ? String.valueOf(word2.charAt(i)) : ""))
+            .collect(java.util.stream.Collectors.joining());
+    }
     
+    public static void main(String[] args) {
+        MergeStringsAlternately mergeStringsAlternately = new MergeStringsAlternately();
+        System.out.println(mergeStringsAlternately.mergeAlternately("abcd", "pq"));
+    }
+
 }
