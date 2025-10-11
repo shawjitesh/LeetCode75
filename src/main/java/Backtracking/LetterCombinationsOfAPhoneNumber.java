0b94129407599a6756661bf7b56fadda2b292c53
shawjitesh <jiteshshaw93@gmail.com>
2025-10-11 12:19:38 +0530

    added skeleton for LetterCombinationOfAPhoneNumber

---
+++ src/main/java/Backtracking/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,16 @@
+package Backtracking;
+
+import java.util.List;
+
+public class LetterCombinationsOfAPhoneNumber {
+
+    public List<String> letterCombinations(String digits) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        LetterCombinationsOfAPhoneNumber obj = new LetterCombinationsOfAPhoneNumber();
+        System.out.println(obj.letterCombinations("23"));
+    }
+    
+}
